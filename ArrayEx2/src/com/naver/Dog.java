3d30b916a7344600cdb84f82341577f2ee9a5837
yewon <hon0760@naver.com>
2020-11-04 17:40:18 +0900

    11월 4일 배운 것 업로드 - array

---
+++ ArrayEx2/src/com/naver/Dog.java
@@ -0,0 +1,26 @@
+package com.naver;
+
+public class Dog {
+	
+	private String name;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dog(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
